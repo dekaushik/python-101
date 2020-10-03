cac8d128ff3e34c8250e3aea2bfba27f06819c39
dekaushik <edgecutterxx@gmail.com>
2020-10-04 00:31:57 +0530

    More commits

--- code/hello_world.py
+++ code/hello_world.py
@@ -5,3 +5,20 @@ print(a)
 # Create a variable of type integer and assign value greater than 100
 # Create a variable of type string and assign a value of any country
 # print both variables
+
+a = 101
+b = 'india'
+print(a)
+print(b)
+c = 97.98
+print(c)
+print(a, b, c)
+print('india', 97.98)
+print('deepak', 'kaushik')
+print('deepak' + ' ' + 'kaushik')
+
+#name = input("Please enter your name: ")
+#print("Hello" + ' ' + name)
+print(str(a) + b)
+print(int(c))
+print(str(int(c)) + " " + b)
