755cf6375938081d8c40aee5bbc6a8545124a641
dekaushik <edgecutterxx@gmail.com>
2020-10-04 01:14:21 +0530

    Formula

--- code/add_number_user_input.py
+++ code/add_number_user_input.py
@@ -1,1 +1,9 @@
 # Take two numbers as user input and print addition, subtraction and multiplication of it
+a = int(input("Please enter 1st number: "))
+b = int(input("Please enter 2nd number: "))
+print("Add:", a + b)
+print("Subtract :", a - b)
+print("Product: ", a * b)
+print("Division: ", a / b)
+print("Modulus= ", a % b)
+# Print a^2 + b^2 + 2ab
