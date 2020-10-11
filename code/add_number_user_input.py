d27ec314e84cea00ce462f98707536bf5bf06f30
dekaushik <edgecutterxx@gmail.com>
2020-10-12 00:58:37 +0530

    Example for calling package

--- code/add_number_user_input.py
+++ code/add_number_user_input.py
@@ -1,3 +1,4 @@
+import math
 # Take two numbers as user input and print addition, subtraction and multiplication of it
 a = int(input("Please enter 1st number: "))
 b = int(input("Please enter 2nd number: "))
@@ -8,3 +9,12 @@ print("Division: ", a / b)
 print("Modulus= ", a % b)
 # Print a^2 + b^2 + 2ab
 print("a + b whole square: " , a * a + 2 * a * b + b * b)
+# a^2 is equal to a**2
+print(a**2)
+# floor division equals to //
+print(10//3)
+# math is a default package and floor and ceil are 2 functions of it.
+# In order to call them, we need to use dot notation that is package_name.function_name
+# Since math is not a built in function, we need to import it before calling just like in line no 1, that is import package_name.
+print(math.floor(5.5))
+print(math.ceil(5.5))
--- code/data_type_practice.py
+++ code/data_type_practice.py
@@ -1,2 +1,8 @@
 # Create a list of Integers, Boolean (2 False and 2 True) and Floating points
 # Create a dict which has key name as 1, 2, 3 of type int and value as a, b, c of type string
+list_1 = [1 , 2, False , True , False , True , 12.32 , 3.34]
+dict_1 = {1: "a", 2: "b", 3: "c", "d": 4.5}
+print(dict_1[2])
+print(dict_1["d"])
+print(type(dict_1["d"]))
+print(int(dict_1["d"]))
